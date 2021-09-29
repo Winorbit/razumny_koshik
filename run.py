63bfe06c5b74e4c19d064c10fcac058e43b2dc2f
sergun-36 <sergun_36@mail.ru>
2021-09-29 18:06:27 +0300

    added template to result

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
+from settings import products
 
-app = Flask(__name__)
+
+
+app = Flask(__name__, template_folder = "templates")
 
 @app.route("/", methods = ["get", "post"])
 def show_pages():
     if request.method == 'POST':
-	    return render_template('result.html')
+	    return render_template('result.html', products=products)
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
 
