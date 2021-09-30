ae25ac26ccb87f4f661d6bc14934f5af2b199768
sergun-36 <sergun_36@mail.ru>
2021-09-30 19:09:59 +0300

    added take_user_input

--- run.py
+++ run.py
@@ -4,10 +4,22 @@ from settings import products
 
 app = Flask(__name__, template_folder="templates")
 
+def take_user_input(resuest):
+    address = request.form.get("address")
+    money = request.form.get("money")
+    categories = request.form.getlist("category")
+    user_input={
+        "address": address,
+        "money": money,
+        "categories": categories
+        }
+    return user_input
 
 @app.route("/", methods=["get", "post"])
 def show_pages():
     if request.method == 'POST':
+        user_input=take_user_input(request)
+        print(user_input)#for testing result
         return render_template('result.html', products=products)
     else:
         return render_template('index.html')
